0da3577e9182d1c2912216f7c9ccbd9e6922357c
OmarMWarraich <earthling986@gmail.com>
2023-04-12 09:52:53 +0500

    fix: Remaining Tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_16_155545) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_12_044310) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,29 +27,29 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_16_155545) do
 
   create_table "inventories", force: :cascade do |t|
     t.string "name"
+    t.string "description"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.string "description"
     t.index ["user_id"], name: "index_inventories_on_user_id"
   end
 
   create_table "inventory_foods", force: :cascade do |t|
+    t.bigint "quantity"
+    t.bigint "food_id", null: false
+    t.bigint "inventory_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "inventory_id", null: false
-    t.bigint "food_id", null: false
-    t.bigint "quantity"
     t.index ["food_id"], name: "index_inventory_foods_on_food_id"
     t.index ["inventory_id"], name: "index_inventory_foods_on_inventory_id"
   end
 
   create_table "recipe_foods", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.bigint "quantity"
     t.bigint "food_id", null: false
     t.bigint "recipe_id", null: false
-    t.bigint "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["food_id"], name: "index_recipe_foods_on_food_id"
     t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
@@ -60,14 +60,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_16_155545) do
     t.string "cooking_time"
     t.string "description"
     t.boolean "public"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "role", default: ""
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -79,7 +80,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_16_155545) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.string "role", default: ""
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
